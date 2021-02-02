bf6cd67cd551de5f383de5c1e96683242e970409
kanir-l <kr@kanir.se>
2021-02-02 09:25:33 +0100

    status feature added

--- models/cardSchema.js
+++ models/cardSchema.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const cardSchema = new mongoose.Schema({
     task: { type: String, require: true, minlength: 2, maxlength: 100 },
     description: String,
-    date: { type: Date, default: Date.now }
+    date: { type: Date, default: Date.now },
+    status: { type: String, default: "todo" }
 })
 
 const todoCard = mongoose.model('task', cardSchema)
